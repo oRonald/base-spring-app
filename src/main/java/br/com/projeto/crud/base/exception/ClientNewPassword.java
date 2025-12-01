172f3eeb05b1ada29913ab87f712b80dd9c40624
oRonold <or040918@gmail.com>
2025-12-01 16:05:54 -0300

    fix exception throwing in ClientService

---
+++ src/main/java/br/com/projeto/crud/base/exception/ClientNewPassword.java
@@ -0,0 +1,8 @@
+package br.com.projeto.crud.base.exception;
+
+public class ClientNewPassword extends RuntimeException{
+
+    public ClientNewPassword(String message) {
+        super(message);
+    }
+}
