e4c2b9cb18ada87992d673ca8c6e45153e08eeb3
oRonold <or040918@gmail.com>
2025-12-01 14:41:51 -0300

    initial commit

---
+++ src/test/java/br/com/projeto/crud/base/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.crud.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
