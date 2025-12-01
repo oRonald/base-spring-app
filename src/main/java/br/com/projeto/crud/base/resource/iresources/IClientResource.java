d8a6f784d847302bb6cac4149b254436ca6b12e1
oRonold <or040918@gmail.com>
2025-12-01 15:56:05 -0300

    fix response entity response

--- src/main/java/br/com/projeto/crud/base/resource/iresources/IClientResource.java
+++ src/main/java/br/com/projeto/crud/base/resource/iresources/IClientResource.java
@@ -1,5 +1,6 @@
 package br.com.projeto.crud.base.resource.iresources;
 
+import br.com.projeto.crud.base.database.dto.ClientDetailsDTO;
 import br.com.projeto.crud.base.database.dto.CreateClientDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -9,5 +10,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Validated
 public interface IClientResource {
 
-    ResponseEntity<?> createClient(@Valid CreateClientDTO dto, UriComponentsBuilder builder);
+    ResponseEntity<ClientDetailsDTO> createClient(@Valid CreateClientDTO dto, UriComponentsBuilder builder);
+
 }
